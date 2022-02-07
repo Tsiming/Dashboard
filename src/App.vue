408c67afa1f4ebf9da1cf1a0115f6a3eeb7003b7
TCAProjects <tsiming1907@163.com>
2022-02-07 10:36:46 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "./assets/css/main.css";
+@import "./assets/css/color-dark.css";
+/*@import  "~vue-tabulator/dist/scss/bootstrap/tabulator_bootstrap4";*/
+</style>
